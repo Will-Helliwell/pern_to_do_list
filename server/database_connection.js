97d8290f69c3f2c8c218b8094d56f8736abf75ea
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-02-12 17:53:55 +0000

    Store database connection details in .env

--- server/database_connection.js
+++ server/database_connection.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'willhelliwell',
-  database: 'pern_to_do_list',
+  host: process.env.POSTGRES_HOST,
+  user: process.env.POSTGRES_USER,
+  database: process.env.POSTGRES_DATABASE,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('dotenv').config();
 const pool = require('./database_connection');
 
 const PORT = process.env.PORT || 5000;
