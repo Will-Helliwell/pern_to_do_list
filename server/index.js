c86bd8159d5d13178d314e2a780facd362e4cd3b
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-02-12 18:57:09 +0000

    Create a route for adding a new to_do

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const pool = require('./database_connection');
 
-const PORT = process.env.PORT || 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());
 
 // ROUTES
 // get all to_dos
@@ -12,8 +17,22 @@ app.get('/to_dos', async (req, res) => {
     const all_todos = await pool.query('SELECT * FROM to_do;');
     const first_description = all_todos.rows[0].description;
     res.json(first_description);
-  } catch (err) {
-    res.json(err.message);
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+// add a single to_do
+app.post('/to_dos', async (req, res) => {
+  try {
+    const { description } = req.body;
+    const new_to_do = await pool.query(
+      'INSERT INTO to_do (description) VALUES ($1) RETURNING *',
+      [description]
+    );
+    res.json(new_to_do.rows[0].to_do_id);
+  } catch (error) {
+    res.json(error);
   }
 });
 
